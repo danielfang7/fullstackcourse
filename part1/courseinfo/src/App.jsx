4afaa68bec85aff6dc3fd6fa7faa027a42e983cc
Daniel Fang <daniel@gladetool.com>
2024-04-01 17:05:19 -0700

    Part1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,44 +9,51 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        {props.part} {props.total}
+      </p>
+    </div>
+  )
+}
+
 const Content = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>
-        {props.part}
-      </p>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>
-        {props.total}
-      </p>
-    </div>
-  )
-}
-
-const App = () => {
-  const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-
+  console.log(props)
   return (
     <div>
-      <Header name={course} />
-      <Content part={part1} />
-      <Content part={part2} />
-      <Content part={part3} />
-      Number of exercises <Total total={exercises1 + exercises2 + exercises3} />
+      <Part part={part1} total={exercises1}/>
+      <Part part={part2} total={exercises2}/>
+      <Part part={part3} total={exercises3}/>
+    </div>
+  )
+}
+
+// const Total = (props) => {
+//   console.log(props)
+//   return (
+//     <div>
+//       <p>
+//         {props.total}
+//       </p>
+//     </div>
+//   )
+// }
+
+const App = () => {
+  const course = 'Half Stack application development'
+  return (
+    <div>
+      <Header name={course}/>
+      <Content/>
     </div>
   )
 }
