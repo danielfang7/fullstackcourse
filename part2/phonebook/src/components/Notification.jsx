f4add51c6cbe6fcf80cad24ded8231565b04c88b
Daniel Fang <daniel@gladetool.com>
2024-04-23 11:31:01 -0700

    adding missed file

---
+++ part2/phonebook/src/components/Notification.jsx
@@ -0,0 +1,14 @@
+const Notification = ({ message, isError }) => {
+    if (message === null) {
+        return null
+    }
+
+    const className = isError ? 'error' : 'success';
+    return (
+        <div className={className}>
+            {message}
+        </div>
+    )
+}
+
+export default Notification
